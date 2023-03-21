f3355d3808ded99aa6f8a1408e82db4255f7e126
LucaBarda <53540072+LucaBarda@users.noreply.github.com>
2023-03-21 14:56:19 +0100

    last comments

---
+++ src/main/java/it/polimi/ingsw/controller/PlayerMessage.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw.controller;
+
+import java.sql.Timestamp;
+
+public class PlayerMessage {
+    private final Player player;
+    private final String message;
+    private final Timestamp timestamp;
+
+    public PlayerMessage(Player player, String message, Timestamp timestamp) {
+        this.player = player;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/model/Token.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.model;
+
+public class Token {
+    private Integer value;
+
+    public Token(Integer value) {
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+}
