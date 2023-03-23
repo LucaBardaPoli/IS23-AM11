0e3c87e1653db520640e8e2a18d8eae022f9a244
Michael Alibeaj <117664707+MikeTech01@users.noreply.github.com>
2023-03-23 12:26:35 +0100

    Added 2 methods (checkAdjacency and findAdjacentGroupCards) in order to check the the adjacency of cards in the bookshelf of a certain player.

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -5,14 +5,15 @@ import java.util.List;
 import java.util.Optional;
 
 public class Player {
-    private String nickname;
+    private final String nickname;
     private PersonalGoal personalGoal;
     private Game game;
     private Bookshelf bookshelf;
     private Integer personalGoalPoints;
     private Integer commonGoalPoints;
     private Integer adjacentPoints;
-    private List<Boolean> commonGoalFullfilled;
+    private  List<Boolean> commonGoalFullfilled;
+
 
     public Player(String nickname, PersonalGoal personalGoal, Game game) {
         this.nickname = nickname;
@@ -38,8 +39,10 @@ public class Player {
     }
 
     private boolean isFullBookshelf(){
-        if(this.bookshelf.getFreeCells() == 0) return false;
-        return true;
+        if(this.bookshelf.getFreeCells() == 0){
+            return false;
+        }
+        else return true;
     }
 
     public void insertCards(Integer column, List<CardType> cardList){
@@ -58,11 +61,9 @@ public class Player {
     }
 
     public boolean checkBookshelf(List<CommonGoal> commonGoals) {
-        // Checking Personal Goal Points
+        /* Checking Personal Goal Points */
         Optional<Integer> points = Optional.of(this.personalGoal.checkGoal(this.bookshelf));
-        if(!points.isEmpty()) {
-            this.personalGoalPoints = points.get();
-        }
+        points.ifPresent(integer -> this.personalGoalPoints = integer);
 
         // Checking Common Goal Points
         for(int i = 0; i < 2; i++) {
@@ -74,16 +75,92 @@ public class Player {
             }
         }
         // Checking Adjacency
+        this.adjacentPoints = checkAdjacency(this.bookshelf);
 
 
         // Checking if Bookshelf is full
-        if(this.isFullBookshelf()) return true;
+        return this.isFullBookshelf();
 
-        return false;
     }
 
     public Integer getPoints() {
         return this.adjacentPoints + this.commonGoalPoints + this.personalGoalPoints;
     }
+
+    /**
+     Static method that checks if the player has in his bookshelf adjacency Goals
+     @param bshelf is the bookshelf of the player
+     @return the score based on the number of adjacencies present in the player's bookshelf
+     */
+    public static int checkAdjacency(Bookshelf bshelf) {
+        //int numGroup = 0;
+        int adjacencyPoints = 0;
+
+        int numRow = bshelf.getRows();
+        int numColumn = bshelf.getColumns();
+
+        boolean[][] visited = new boolean[numRow][numColumn];
+
+        for (int i = 0; i < numRow; i++) {
+            for (int j = 0; j < numColumn; j++) {
+                if (!visited[i][j]) {
+                    int groupSize = findAdjacentGroupCards(bshelf, visited, i, j, bshelf.getCell(new Position(i,j)));
+                    if (groupSize >= 3) {
+                        //numGroup++;
+
+                        // Based on the size of the adjacent card group, returns the score (minimum size to earn points is 3)
+                        if(groupSize == 3){
+                            adjacencyPoints += 2;
+                        } else if(groupSize == 4){
+                            adjacencyPoints += 3;
+                        } else if(groupSize == 5){
+                            adjacencyPoints += 5;
+                        } else adjacencyPoints += 8;
+
+                    }
+                }
+            }
+        }
+
+        return adjacencyPoints;
+    }
+
+
+    /**
+     Finds the Adjacent Cards present in the bookshelf
+     @param bshelf is the bookshelf of the player
+     @param visited is a matrix of bool which takes into account whether  a Card has been visited or not
+     @param row is the index of the row of the card that should be checked
+     @param column is the index of the column of the Card that should be checked
+     @param type is the type of the card that should be checked
+     @return the size of the group with cards of the same type
+     */
+    private static int findAdjacentGroupCards(Bookshelf bshelf, boolean[][] visited, int row, int column, Optional<CardType> type) {
+
+        Position posToCheck = new Position(row, column);
+
+        // Check out of bounds + Alredy Visited
+        if ( (row < 0 || row >= 6 || column < 0 || column >= 5) || visited[row][column]) {
+            return 0;
+        } else if( bshelf.getCell(posToCheck).isEmpty() || type.isEmpty()){
+            return 0;
+        } else if( bshelf.getCell(posToCheck).get() != type.get() ){
+            return 0;
+        }
+
+        visited[row][column] = true;
+
+        int groupSize = 1;
+
+
+        // Visit locations adjacent to the position provided - ( it doesn't check for the diagonal e.g. row +-1, column +-1 && row+-1, column -+ 1)
+        groupSize += findAdjacentGroupCards(bshelf, visited, row - 1, column, type);
+        groupSize += findAdjacentGroupCards(bshelf, visited, row + 1, column, type);
+        groupSize += findAdjacentGroupCards(bshelf, visited, row, column - 1, type);
+        groupSize += findAdjacentGroupCards(bshelf, visited, row, column + 1, type);
+
+        return groupSize;
+    }
+
 }
 
